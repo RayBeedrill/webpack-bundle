77897f0ee737404e3a405a271985bcbc27d9cb42
Raybeedrill <demezis1994@gmal.com>
2018-04-25 02:50:03 +0300

    more testts

--- src/js/Game.js
+++ src/js/Game.js
@@ -2,17 +2,34 @@
 export default class Game {
   _board: Array<Array<string>>;
   _userMoveSymbol: string;
+  _computerMoveSymbol: string;
   constructor(): void {
     this._board = [["", "", ""], ["", "", ""], ["", "", ""]];
     this._userMoveSymbol = "x";
+    this._computerMoveSymbol = "o";
   }
   getState(): Array<Array<string>> {
     return this._board;
   }
   acceptUserMove(x: number, y: number): void {
-    this._updateBoard(x, y);
+    if (this._isCellFree(x, y)) {
+      this._updateBoard(x, y, { moveSymbol: this._userMoveSymbol });
+    } else {
+      return this._throwException("cell is already taken");
+    }
   }
-  _updateBoard(x: number, y: number) {
-    this._board[x][y] = this._userMoveSymbol;
+  createComputerMove(): void {
+    this._updateBoard(0, 0, { moveSymbol: this._computerMoveSymbol });
+  }
+  _updateBoard(x: number, y: number, config: { moveSymbol: string }): void {
+    const { moveSymbol = this._userMoveSymbol } = config;
+    this._board[x][y] = moveSymbol;
+  }
+  _isCellFree(x: number, y: number): boolean {
+    return !this._board[x][y];
+  }
+
+  _throwException(msg: string): void {
+    throw new Error(msg);
   }
 }
--- src/js/test/index.test.js
+++ src/js/test/index.test.js
@@ -28,4 +28,18 @@ describe("Game", () => {
 
     chai.expect(board[x][y]).to.equal(userMoveSymbol);
   });
+  it("Trows exception if user moves in taken sell", () => {
+    const x: number = 2,
+      y: number = 2;
+
+    game.acceptUserMove(x, y);
+    const func = game.acceptUserMove.bind(game, x, y);
+    chai.expect(func).to.throw("cell is already taken");
+  });
+  it("Computer moves in top left cell", () => {
+    game.createComputerMove();
+    const board: Array<Array<string>> = game.getState();
+
+    chai.expect(board[0][0]).to.equal("o");
+  });
 });
