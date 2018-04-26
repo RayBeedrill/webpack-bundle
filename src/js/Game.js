42b0d6811fe29039546aa844a1f1337d63d90640
Raybeedrill <demezis1994@gmal.com>
2018-04-27 02:57:56 +0300

    das

--- src/js/Game.js
+++ src/js/Game.js
@@ -3,23 +3,30 @@ export default class Game {
   _board: Array<Array<string>>;
   _userMoveSymbol: string;
   _computerMoveSymbol: string;
+  _history: Array<mixed>;
   constructor(): void {
     this._board = [["", "", ""], ["", "", ""], ["", "", ""]];
     this._userMoveSymbol = "x";
     this._computerMoveSymbol = "o";
+    this._history = [];
   }
   getState(): Array<Array<string>> {
     return this._board;
   }
   acceptUserMove(x: number, y: number): void {
     if (this._isCellFree(x, y)) {
+      this._history.push({ turn: "user", x, y });
       this._updateBoard(x, y, { moveSymbol: this._userMoveSymbol });
     } else {
       return this._throwException("cell is already taken");
     }
   }
   createComputerMove(): void {
+    this._history.push({ turn: "computer", x: 0, y: 0 });
     this._updateBoard(0, 0, { moveSymbol: this._computerMoveSymbol });
+  }
+  getMoveHistory(): Array<mixed> {
+    return this._history;
   }
   _updateBoard(x: number, y: number, config: { moveSymbol: string }): void {
     const { moveSymbol = this._userMoveSymbol } = config;
--- src/js/test/index.test.js
+++ src/js/test/index.test.js
@@ -1,6 +1,9 @@
 //@flow
 import chai from "./../../../node_modules/chai/";
 import Game from "../Game.js";
+
+const userName: string = "user";
+const computerName: string = "computer";
 const userMoveSymbol: string = "x";
 const initialGameBoard: Array<Array<string>> = [
   ["", "", ""],
@@ -41,5 +44,31 @@ describe("Game", () => {
     const board: Array<Array<string>> = game.getState();
 
     chai.expect(board[0][0]).to.equal("o");
+  });
+  it("Game saves user's move in history", () => {
+    const x: number = 1,
+      y: number = 1;
+    game.acceptUserMove(x, y);
+    const history: Array<mixed> = game.getMoveHistory();
+
+    chai.expect(history).to.deep.equal([{ turn: userName, x, y }]);
+  });
+  it("Game saves computer's move in history", () => {
+    game.createComputerMove();
+
+    const history: Array<mixed> = game.getMoveHistory();
+
+    chai.expect(history).to.deep.equal([{ turn: computerName, x: 0, y: 0 }]);
+  });
+  it("Game saves 1 computer's move and 1 user's move in history", () => {
+    const x: number = 1,
+      y: number = 1;
+    game.createComputerMove();
+    game.acceptUserMove(x, y);
+    const history: Array<any> = game.getMoveHistory();
+
+    chai.expect(history.length).to.equal(2);
+    chai.expect(history[0].turn).to.equal(computerName);
+    chai.expect(history[1].turn).to.equal(userName);
   });
 });
